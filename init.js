eac27d6c00de21f3dae2162aa5b874fed5126434
Tanuj <tanujnay112@live.com>
2016-06-27 11:09:22 -0700

    Added files

---
+++ init.js
@@ -0,0 +1,10 @@
+var clientId = "568327038315-5gc1eovtfcrgpco0sumpbhfaffhi93ja.apps.googleusercontent.com";
+var config = {
+      apiKey: "AIzaSyC5vrJU4FkBRmGzAYMvtqYXu3F06OC7bQM",
+      authDomain: "science-bowl.firebaseapp.com",
+      databaseURL: "https://science-bowl.firebaseio.com",
+      storageBucket: "science-bowl.appspot.com",
+    };
+var LOGIN = "index.html";
+var ENTRY = "entry.html";
+var CONF = "confirmation.html";
