13cc0682695d9c296ae24c14a3df4ee73919d464
TNayak <tanujnay112@live.com>
2016-08-15 12:44:41 -0700

    Fixed delete issue in editor in review.html

--- init.js
+++ init.js
@@ -14,19 +14,19 @@ function populate(s1, s2){
     s2.innerHTML="";
     var optionArray;
     if(s1.value == "Biology"){
-        optionArray = ["Anatomy|Anatomy","Plant Biology|Plant Biology", "Ecology|Ecology"];
+        optionArray = ["Anatomy_Physiology|Anatomy & Physiology","Plant_Biology|Plant & Biology", "Cellular_Microbiology|Cellular & Microbiology", "Genetics_Evolution|Genetics & Evolution","Misc|Misc"];
     }else if(s1.value == "Chemistry"){
-        optionArray = ["Sub1|Sub1","Sub2|Sub2","Sub3|Sub3","Sub4|Sub4","Sub5|Sub5"];
+        optionArray = ["Organic|Organic","Electrochemistry|Electrochemistry","Thermochemistry|Thermochemistry","General_Reactions|General Reactions","Periodic Table_Atomic Structure|Periodic Table & Atomic Structure","Misc|Misc"];
     }else if(s1.value == "Physics"){
-        optionArray = ["Sub1|Sub1","Sub2|Sub2","Sub3|Sub3","Sub4|Sub4","Sub5|Sub5"];
-    }else if(s1.value == "Earth Science"){
-        optionArray = ["Sub1|Sub1","Sub2|Sub2","Sub3|Sub3","Sub4|Sub4","Sub5|Sub5"];
+        optionArray = ["Mechanics|Mechanics","Thermodynamics|Thermodynamics","Electricity_Magnetism|Electricity & Magnetism","Fluid Dynamics|Fluid Dynamics","Nuclear|Nuclear","Misc|Misc"];
     }else if(s1.value == "Astronomy"){
-        optionArray = ["Sub1|Sub1","Sub2|Sub2","Sub3|Sub3","Sub4|Sub4","Sub5|Sub5"];
+        optionArray = ["Astrometry|Astrometry","Stars|Stars","Cosmology|Cosmology","Planets|Planets","Observation|Observation","Extra-solar_Bodies|Extra-solar Bodies","Misc|Misc"];
+    }else if(s1.value == "Earth Science"){
+        optionArray = ["Rocks_Minerals|Rocks & Minerals","Geologic_Time|Geologic Time","Water_Bodies|Water Bodies","Natural_Disasters|Natural Disasters","Weather|Weather", "Crust_Ice_Bodies|Crust & Ice Bodies", "Misc|Misc"];
     }else if(s1.value == "Energy"){
-        optionArray = ["Sub1|Sub1","Sub2|Sub2","Sub3|Sub3","Sub4|Sub4","Sub5|Sub5"];
+        optionArray = ["Renewable|Renewable","Non-Renewable|Non-Renewable","Statistics|Statistics","Applications|Applications","Misc|Misc"];
     }else if(s1.value == "Math"){
-        optionArray = ["Sub1|Sub1","Sub2|Sub2","Sub3|Sub3","Sub4|Sub4","Sub5|Sub5"];
+        optionArray = ["Calculus|Calculus","Geometry|Geometry","Algebra|Algebra","Misc|Misc"];
     }
     for(var option in optionArray){
 		var pair = optionArray[option].split("|");
